2b874f22ca9df28a7f7f212ff423172af9541375
Nghia Nguyen <aihgn224@gmail.com>
2022-04-29 19:47:54 -0700

    improved interface

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,30 +1,31 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 function GalleryItem(props) {
     let [view, setView] = useState(false)
 
-    const simpleStyle = {
-        'width': '25vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px'
-    }
-    
-    const detailStyle = {
-        'width': '80vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px',
-        'backgroundImage': `url(${props.item.artworkUrl100})`,
-        'backgroundRepeat': 'no-repeat',
-        'backgroundSize': 'cover',
-        'color': 'yellow'
-    }
-    
-
     const simpleView = () => {
+        const simpleStyle = {
+            'width': '25vw',
+            'height': '20vh',
+            'border': '1px solid black',
+            'margin': '2px'
+        }
+        
+        const detailStyle = {
+            'width': '80vw',
+            'height': '20vh',
+            'border': '1px solid black',
+            'margin': '2px',
+            'backgroundImage': `url(${props.item.artworkUrl100})`,
+            'backgroundRepeat': 'no-repeat',
+            'backgroundSize': 'cover',
+            'color': 'yellow'
+        }
+        
         return (
-            <div style={simpleStyle}>
+            <div style ={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -33,21 +34,34 @@ function GalleryItem(props) {
 
     const detailView = () => {
         return (
-            <div>
+            <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
         )
     }
+    
 
     return (
-        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
-            <p>One Gallery Item</p>
-                {view ? detailView() : simpleView()}
+        <div onClick={() => setView(!view)}
+        style={{'display': 'inline-block'}}>
+        
+            {/* This simple ternary shows the simple view when 'view' is false! */}
+            {view ? detailView() : simpleView()}
+
         </div>
     )
-}
 
+}
 export default GalleryItem
